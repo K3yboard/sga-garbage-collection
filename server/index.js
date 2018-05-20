4bff3dc59fc9811c3da29ccbcb5ef31f62ea62bf
Paulo Pereira dos Anjos <paulopereiradosanjos@gmail.com>
2018-05-20 14:05:43 -0300

    criando metodo para listas clientes

---
+++ server/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 3000; //porta padrão
+const mysql = require('mysql');
+
+//configurando o body parser para pegar POSTS mais tarde
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//definindo as rotas
+const router = express.Router();
+router.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
+app.use('/', router);
+
+//inicia o servidor
+app.listen(port);
+console.log('API funcionando http://localhost:3000/!');
+
+function execSQLQuery(sqlQry, res) {
+    const connection = mysql.createConnection({
+        host     : 'localhost',
+        user     : 'root',
+        password : '',
+        database : 'test'
+    });
+
+    connection.query(sqlQry, function(error, results, fields){
+        if(error) 
+            res.json(error);
+        else
+            res.json(results);
+        connection.end();
+        console.log('executou!');
+    });
+}
+
+router.get('/clientes/:id?', (req, res) =>{
+    let filter = '';
+    if(req.params.id) filter = ' WHERE ID=' + parseInt(req.params.id);
+    execSQLQuery('SELECT * FROM Clientes' + filter, res);
+});
