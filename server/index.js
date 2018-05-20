54b25573939144a3a15dad4c1163d470bf9468ab
Paulo Pereira dos Anjos <paulopereiradosanjos@gmail.com>
2018-05-20 19:11:26 -0300

    Criando CRUD dos contratos

--- server/index.js
+++ server/index.js
@@ -63,12 +63,51 @@ router.patch('/empresa/:id', (req, res) =>{
     const id = parseInt(req.params.id);
     const nome_empresa = req.body.nome_empresa.substring(0,150);
     const cnpj_empresa = req.body.cnpj_empresa.substring(0,14);
+    const inscricao_municipal = req.body.inscricao_municipal.substring(0,50);
+    const licensa = req.body.licensa.substring(0,50);
     execSQLQuery(`
-        UPDATE Clientes SET 
+        UPDATE empresa SET 
         NOME_EMPRESA='${nome_empresa}',
         CNPJ_EMPRESA='${cnpj_empresa}',
         INSCRICAO_MUNICIPAL='${inscricao_municipal}',
         LICENSA='${licensa}'
         WHERE ID_EMPRESA=${id}
     `, res);
-})
+});
+
+// CONTRATO
+
+router.get('/contrato/:id?', (req, res) => {
+    let filter = '';
+    if(req.params.id) filter = ' WHERE ID_CONTRATO=' + parseInt(req.params.id);
+    execSQLQuery('SELECT * FROM contrato' + filter, res);
+});
+
+router.delete('/contrato/:id', (req, res) => {
+    execSQLQuery('DELETE FROM contrato WHERE ID_CONTRATO=' + parseInt(req.params.id), res);
+});
+
+router.post('/contrato', (req, res) => {
+    const data_contrato = req.body.data_contrato;
+    const data_vencimento_contrato = req.body.data_vencimento_contrato;
+    const empresa_id_empresa = req.body.empresa_id_empresa;
+    execSQLQuery(`
+        INSERT INTO 
+        contrato(DATA_CONTRATO, DATA_VENCIMENTO_CONTRATO, EMPRESA_ID_EMPRESA)
+        VALUES('${data_contrato}','${data_vencimento_contrato}', '${empresa_id_empresa}')
+    `, res);
+});
+
+router.patch('/contrato/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const data_contrato = req.body.data_contrato;
+    const data_vencimento_contrato = req.body.data_vencimento_contrato;
+    const empresa_id_empresa = req.body.empresa_id_empresa;
+    execSQLQuery(`
+        UPDATE contrato SET 
+        DATA_CONTRATO='${data_contrato}',
+        DATA_VENCIMENTO_CONTRATO='${data_vencimento_contrato}',
+        EMPRESA_ID_EMPRESA='${empresa_id_empresa}'
+        WHERE ID_CONTRATO=${id}
+    `, res);
+});
