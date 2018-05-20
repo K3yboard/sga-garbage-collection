a470a8eaab80f025d19773bd9d1225be60e842a0
Paulo Pereira dos Anjos <paulopereiradosanjos@gmail.com>
2018-05-20 14:20:04 -0300

    Atualizando cliente

--- server/index.js
+++ server/index.js
@@ -50,3 +50,10 @@ router.post('/clientes', (req, res) => {
     const cpf = req.body.cpf.substring(0,11);
     execSQLQuery(`INSERT INTO Clientes(Nome, CPF) VALUES('${nome}','${cpf}')`, res);
 });
+
+router.patch('/clientes/:id', (req, res) =>{
+    const id = parseInt(req.params.id);
+    const nome = req.body.nome.substring(0,150);
+    const cpf = req.body.cpf.substring(0,11);
+    execSQLQuery(`UPDATE Clientes SET Nome='${nome}', CPF='${cpf}' WHERE ID=${id}`, res);
+})
