4a7b8f9334f9b66d57951a53c369abcb71e7b734
Paulo Pereira dos Anjos <paulopereiradosanjos@gmail.com>
2018-05-27 20:08:42 -0300

    Criando metodos para atualizar valor e salvar o contrato

--- projeto/src/app/form/contrato-form/contrato-form.component.ts
+++ projeto/src/app/form/contrato-form/contrato-form.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AppService } from '../../app.service';
+import { Contrato } from '../../model/contrato.model';
 
 @Component({
   selector: 'app-contrato-form',
@@ -7,4 +9,41 @@ import { Component } from '@angular/core';
 })
 export class ContratoFormComponent {
 
+  constructor(private appService: AppService) {}
+
+  public contrato: Contrato = new Contrato('', '');
+
+  public dataContrato: string;
+  public dataVencimento: string;
+
+  public botao: boolean = true;
+
+  atualizarDataContrato(data: string): void {
+    this.dataContrato = data;
+    this.habilitaBotao();
+  }
+
+  atualizarDataVencimento(data: string): void {
+    this.dataVencimento = data;
+    this.habilitaBotao();
+  }
+
+  habilitaBotao(): void {
+    if(this.dataContrato != '' && this.dataVencimento != '') {
+      this.botao = false;
+    } else {
+      this.botao = true;
+    }
+  }
+
+  salvar(): void {
+    this.contrato.data_contrato = this.dataContrato;
+    this.contrato.data_vencimento_contrato = this.dataVencimento;
+    this.appService.salvarContrato(this.contrato)
+      .subscribe(
+        (contrato) => console.log(contrato),
+        (erro) => console.log(erro),
+        () => console.log('Ok')
+      );
+  }
 }
